4ea49822f0ce4594a40499597bc3b71255444646
rezihardi <rezydoerasta@gmail.com>
2022-01-14 00:27:14 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger} from "@nestjs/common";
 
@@ -8,6 +9,15 @@ const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('SWAGGER EXAMPLE')
+    .setDescription('The API SWAGGER FOR NEST ')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 
   Logger.log(`running di port ${port}`, `RUNNING PORT`);
